793d1c85c90d8265657fa224d522511b52b668ae
Sourav Sharma <sourav.sharma.600@gmail.com>
2021-03-23 10:58:21 +0530

    gallary and news modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { Gallery1Component } from './gallery1/gallery1.component';
+import { NewsComponent } from './news/news.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    Gallery1Component,
+    NewsComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule
